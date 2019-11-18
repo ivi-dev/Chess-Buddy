f529cd383a950fa10e90dbd88b07050d3d127a72
Iliyan Videv <videviliyan@gmail.com>
2019-11-18 11:17:36 +0200

    Built the app

---
+++ build/precache-manifest.0a675c347a41c570798d0a47f5da99f6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce877578076b6b1faee51f8c3750bd21",
+    "url": "/index.html"
+  },
+  {
+    "revision": "306ea591033704d512dc",
+    "url": "/static/css/main.5fcbc960.chunk.css"
+  },
+  {
+    "revision": "f5c714a6bd5fdb227d18",
+    "url": "/static/js/2.97319559.chunk.js"
+  },
+  {
+    "revision": "306ea591033704d512dc",
+    "url": "/static/js/main.b58de2ea.chunk.js"
+  },
+  {
+    "revision": "e105011e003ebb74e624",
+    "url": "/static/js/runtime-main.95d19b34.js"
+  }
+]);
